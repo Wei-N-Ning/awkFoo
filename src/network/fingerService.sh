dce18254fe62734f0c74bfc371bc0f0f7c60848d
Wei Ning <wning@wetafx.co.nz>
2018-03-13 10:55:22 +1300

    - more network examples

---
+++ src/network/fingerService.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+# note how it uses a variable "NetService" to refer the special file path
+# not working in Wt vm (vdi) environment
+function run() {
+    echo "" | awk '
+BEGIN {
+    NetService = "/inet/tcp/0/localhost/cfinger"
+    print "" |& NetService
+    while ((NetService |& getline) > 0) {
+        print $0
+    }
+    close(NetService)
+}
+'
+}
+
+run
