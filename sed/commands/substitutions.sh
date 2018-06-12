21d7898f2a9940ca61ae234bd44860ae189d1202
Wei Ning <nwcgnw@gmail.com>
2018-06-12 22:16:15 +1200

    - sed in-place substitution example

--- sed/commands/substitutions.sh
+++ sed/commands/substitutions.sh
@@ -3,7 +3,7 @@
 # source:
 # gnu.org: sed, a stream editor
 
-function buildSUT() {
+buildSUT() {
     sut=/tmp/_
     echo "there is a cow
 thereisacow
@@ -14,11 +14,15 @@ do the right thing
 break 1 if app.m_d == 13" > ${sut}
 }
 
-
-
-function run() {
-    echo
-
+# replace "sub_hex" string "%function_address%"
+# $1: optional arguments
+run() {
+    sed ${1} 's/sub_0x[0-9]\+/\%function_address\%/g' ${sut} | awk '/function_/ {print}'
 }
 
+buildSUT
 run
+
+# substitution in-place
+run -i
+cat ${sut} | awk '/function_/ {print}'
